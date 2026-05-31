53972d7b176d4106868cc8ac9cf4c80536401fd4
James Johns <192176108+nahmanmate@users.noreply.github.com>
2026-05-31 14:09:00 +1000

    test: add payload_only testdata for non-atom variants

---
+++ transpiler/testdata/payload_only.union.go
@@ -0,0 +1,24 @@
+package main
+
+type redConfig struct{ r int }
+type blueConfig struct{ b int }
+type greenConfig struct{ g int }
+
+type color union {
+	red   redConfig
+	blue  blueConfig
+	green greenConfig
+}
+
+func pick(c color) int {
+	switch v := c.(union) {
+	case red:
+		return v.r
+	case blue:
+		return v.b
+	case green:
+		return v.g
+	default:
+	}
+	return 0
+}
